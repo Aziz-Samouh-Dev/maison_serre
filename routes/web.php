3cdae57bcce0a6024dc9285a4f594df4eeda0ec0
Aziz-Samouh-Dev <asamouh31@gmail.com>
2024-05-31 05:46:41 -0700

    o

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,9 @@ use App\Http\Controllers\DashboardController;
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/data', [DashboardController::class, 'fetchSensorData']);
-Route::post('/actuator/{id}', [DashboardController::class, 'updateActuator'])->name('dashboard.updateActuator');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard/data', [DashboardController::class, 'fetchSensorData']);
+Route::post('/dashboard/actuator/{id}', [DashboardController::class, 'updateActuator'])->name('dashboard.updateActuator');
